4d01edc8130dcdac76115ef8ad7151c93377a188
nauvalachid <nauvalachid@gmail.com>
2023-03-27 10:45:18 +0700

    menampilkan penilaian

--- Exercise/Exercise.cpp
+++ Exercise/Exercise.cpp
@@ -49,5 +49,5 @@ int main() {
 	//cout << "Status kelulusan = ' << st;
 	//cout << "Status kelulusan = " << status(rata);
 	cout << "Status kelulusan = " << status(rerata(nilM, nilB);
-	return 0;
+	return 0; 
 }
